8c0cb124edbbf4f3589729dd33030e8e0a3611e9
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-09-23 16:15:33 +0800

    using jxl

---
+++ src/core/DutyTimeTable.java
@@ -0,0 +1,9 @@
+package core;
+
+public class DutyTimeTable {
+	String path;
+
+	public DutyTimeTable(String path) {
+		this.path = path;
+	}
+}
---
+++ src/test/DutyTimeTableTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import core.DutyTimeTable;
+
+public class DutyTimeTableTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String path = "";
+		DutyTimeTable timeTable = new DutyTimeTable(path);
+	}
+
+}
