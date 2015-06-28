a83962849367257097725e45691620730d1e7b4c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-06-29 00:04:07 +0800

    rebuilding scene using relative scaling (when window resize)

---
+++ src/dutytimetable/view/MainSceneLauncher.java
@@ -0,0 +1,22 @@
+package dutytimetable.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by beenotung on 6/8/15.
+ */
+public class MainSceneLauncher extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Auto Duty-Timetable Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
